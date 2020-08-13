4c08b98faa29de8e00afcda8382d7749bbb50bac
TheIcySpark <saacmanjarrez@gmail.com>
2020-08-13 05:03:05 -0700

    Command: se implemento la funcion de hacer y des hacer por medio de comandos

---
+++ Assets/Scenes/Command/Hacer DesHacer/ComandoV3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NComandoV3
+{
+    public abstract class ComandoV3
+    {
+        public abstract void Hacer(ControladorObjeto controladorObjeto, Vector3 pos);
+        public abstract void Deshacer(ControladorObjeto controladorObjeto);
+    }
+
+    public class ComandoMoverV3: ComandoV3
+    {
+        public Vector3 posicion;
+        public override void Hacer(ControladorObjeto controladorObjeto, Vector3 pos)
+        {
+            posicion = pos;
+            controladorObjeto.Mover(pos);
+        }
+
+        public override void Deshacer(ControladorObjeto controladorObjeto)
+        {
+            controladorObjeto.Mover(-posicion);
+        }
+    }
+}
+
